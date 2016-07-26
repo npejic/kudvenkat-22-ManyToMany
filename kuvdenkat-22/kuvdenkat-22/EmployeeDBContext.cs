e20f73bf30c8eb0d35d0ff3fec31611909aed27a
nik <npejic@gmail.com>
2016-07-26 17:15:25 +0200

    ne radi RemoveButton

---
+++ kuvdenkat-22/kuvdenkat-22/EmployeeDBContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace kuvdenkat_22
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+            .HasMany(t => t.Courses)
+            .WithMany(t => t.Students)
+            .Map(m =>
+            {
+                m.ToTable("StudentCourses");
+                m.MapLeftKey("StudentID");  //LeftKey mora biti naziv one klase koja se spominje u modelBuilder.Entity<>(...) metodi
+                m.MapRightKey("CourseID");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ kuvdenkat-22/kuvdenkat-22/StudentCourses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kuvdenkat_22
+{
+    public class StudentCourses
+    {
+        public int StudentID { get; set; }
+        public int CourseID { get; set; }
+    }
+}
